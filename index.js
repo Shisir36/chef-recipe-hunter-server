9b0485380bb1c17978fcd753005038f8d6a69975
Al Mamun <almamunshisir770@gmail.com>
2023-05-02 20:43:20 +0600

    modify the Json file and create and set another server to get data

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors")
 const chefData = require("./Data/chef.json")
+const recipes = require("./Data/recipes.json")
 app.use(cors())
 app.get('/', (req, res) => {
 res.send("data is coming")
@@ -10,7 +11,18 @@ res.send("data is coming")
 app.get("/chef", (req, res) => {
     res.send(chefData)
 })
-
+app.get("/recipes", (req, res) => {
+    res.send(recipes)
+})
+app.get("/recipes/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const recipe = recipes.filter((r) => r.id === id);
+    if (recipe) {
+      res.send(recipe);
+    } else {
+      res.status(404).send("Recipe not found");
+    }
+  });
 app.listen(port, ()=>{
     console.log(`data is coming ${port}`);
 })
